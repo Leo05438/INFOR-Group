539b36a1ae89c109714dea4024a4d97f51bc665d
Pooh1223 <leo891223@gmail.com>
2018-04-14 22:42:25 +0800

    InforApp
    Working Progress: 15%

---
+++ InforApp/app/src/main/java/com/example/leolin/inforapp/PersonalProfilePage.java
@@ -0,0 +1,13 @@
+package com.example.leolin.inforapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_profile_page);
+    }
+}
