cabcecd12b5c8e28b960a843e48e4ed131e69b19
archerop <chen61306@gmail.com>
2018-06-03 12:27:32 +0800

    sssss
    s

--- infor-group/routes/index.js
+++ infor-group/routes/index.js
@@ -9,13 +9,28 @@ var Categories = mongoose.model('Category');
 //home page
 router.get('/', function(req, res, next) {
 
-    res.locals.username = req.session.name ;
+    res.locals.username = req.session.name;
     res.locals.authenticated = req.session.logined;
-    Categories.find().lean().exec(function(e,docs){
+     Categories.find().lean().exec(function(e,docs){
       res.locals.arr = docs;
       res.locals.length = docs.length;
       res.locals.i = 0;
-      res.render( 'index');
+      if(req.session.logined){
+      	Users.findOne({username:req.session.name},function(err,doc){
+      		if(err){
+      			console.log("nononojizz")
+      		}else{
+      			res.locals.usericon=doc.icon
+      			res.render('index');
+      		}
+      		
+      	})
+    	
+    }else{
+    	res.render( 'index');
+    }
+
+      
     });
 
 
